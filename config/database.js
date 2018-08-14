ce42570a3c5ffadedf87733578c9b4fe3e7d210e
Ley21 <andreas.spuling@web.de>
2018-08-14 07:58:27 +0000

    add database connection for mongodb

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url : 'mongodb://'+process.env.db_user+':'+process.env.db_passwort+'@'+process.env.db_address+'/snsubs_ddl' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
