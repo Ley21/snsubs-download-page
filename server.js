2c05713fef642e6e27b7bc20abfab90bdc1251e6
Ley21 <andreas.spuling@web.de>
2018-08-14 07:37:06 +0000

    Fist commit, with login dialog and template framework

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+app.use(session({ secret: 'shinigaminekosubsisnotdead' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./app/routes.js')(app, passport);
+
+
+app.listen(port);
+console.log(port + ' is the magic port');
